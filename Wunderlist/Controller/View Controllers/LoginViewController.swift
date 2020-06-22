f9b49274108cae825dc913e8e9b333a4724b7cb2
Dahna <dbuenrostro09@gmail.com>
2020-06-22 16:07:58 -0400

    Reconnected outlets and recreated files

---
+++ Wunderlist/Controller/View Controllers/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  LoginViewController.swift
+//  Wunderlist
+//
+//  Created by Dahna on 6/22/20.
+//  Copyright © 2020 Hazy Studios. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet var loginSegControl: UISegmentedControl!
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    
+    // MARK: - Actions
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
