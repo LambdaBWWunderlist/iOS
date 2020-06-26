2074a25ba38f68ef619c3a7eadc7f42af610474b
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-06-26 05:56:41 -0700

    alerting user when login fails

--- Wunderlist/Controller/View Controllers/LoginViewController.swift
+++ Wunderlist/Controller/View Controllers/LoginViewController.swift
@@ -14,9 +14,8 @@ enum LoginType {
 }
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
-    
+
     // MARK: - Properties
-    
     weak var delegate: TodoListTableViewController?
     var loginType = LoginType.register
     var togglePassword: Bool = false
@@ -24,29 +23,28 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     let hidePasswordImage = UIImage(systemName: "eye.slash")?.withTintColor(UIColor.buttonOrange, renderingMode: .alwaysOriginal)
     let showPasswordImage = UIImage(systemName: "eye")?.withTintColor(UIColor.buttonOrange, renderingMode: .alwaysOriginal)
     let authService = AuthService()
-    
+
     // MARK: - Outlets
-    
     @IBOutlet var loginSegControl: UISegmentedControl!
     @IBOutlet var usernameTextField: UITextField!
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var loginButton: UIButton!
     @IBOutlet var promptLabel: UILabel!
-    
+
     // MARK: - Actions
     @IBAction func loginButtonTapped(_ sender: Any) {
-        
+
         guard let username = usernameTextField.text,
             !username.isEmpty,
-            
+
             let password = passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines),
             !password.isEmpty else { return }
-        
+
         if loginType == .register {
             guard let email = emailTextField.text,
                 !email.isEmpty else { return }
-            
+
             authService.registerUser(username: username, password: password, email: email) {
                 DispatchQueue.main.async {
                     self.alertWithMessage(title: "Registration Complete", message: "Please Log In")
@@ -58,21 +56,25 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
                 }
             }
         } else if loginType == .login {
-            authService.loginUser(with: username, password: password) {
-                DispatchQueue.main.async {
-                    print("\(String(describing: AuthService.activeUser))")
-                    self.delegate?.updateViews()
-                    self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            authService.loginUser(with: username, password: password) { [weak self] status in
+                guard let self = self else { return }
+                if status {
+                    DispatchQueue.main.async {
+                        print("\(String(describing: AuthService.activeUser))")
+                        self.delegate?.updateViews()
+                        self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                    }
+                } else {
+                    self.alertWithMessage(title: "Login Failed", message: "Please check your password")
                 }
-
             }
 
         }
-        
+
     }
-    
+
     @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
-        
+
         if sender.selectedSegmentIndex == 0 {
             // sign up
             loginType = .register
@@ -87,54 +89,42 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             promptLabel.text = "Please Log In"
         }
     }
-    
-    
-    // MARK: - Lifecycle
-    override func viewDidLoad() {
-        
-//        User(identifier: 7, username: AuthService.testUser.username, email: "thehammersvpa@gmail.com")
-//        do {
-//            try CoreDataStack.shared.save()
-//        } catch {
-//        print("\(error) saving CoreData user")
-//        }
-        let authService = AuthService()
 
-        authService.loginUser(with: AuthService.testUser.username, password: AuthService.testUser.password!) {
-            print("logged in")
-        }
-        
+
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+
         toggleButton.setImage(hidePasswordImage, for: .normal)
         super.viewDidLoad()
-        
+
         configureLoginView()
         self.passwordTextField.delegate = self
     }
-    
+
     // MARK: - Functions
     private func configureLoginView() {
-        
+
         loginButton.layer.cornerRadius = 6.0
         passwordTextField.isSecureTextEntry = true
-        
+
         loginButton.layer.borderColor = UIColor.accentBlue.cgColor
         loginButton.layer.borderWidth = 1.0
-        
+
         usernameTextField.layer.borderColor = UIColor.accentBlue.cgColor
         usernameTextField.borderStyle = .roundedRect
         usernameTextField.layer.cornerRadius = 6.0
         usernameTextField.layer.borderWidth = 1.0
-        
+
         emailTextField.layer.borderColor = UIColor.accentBlue.cgColor
         emailTextField.borderStyle = .roundedRect
         emailTextField.layer.cornerRadius = 6.0
         emailTextField.layer.borderWidth = 1.0
-        
+
         passwordTextField.layer.borderColor = UIColor.accentBlue.cgColor
         passwordTextField.borderStyle = .roundedRect
         passwordTextField.layer.cornerRadius = 6.0
         passwordTextField.layer.borderWidth = 1.0
-        
+
         toggleButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -16, bottom: 0, right: 0)
         toggleButton.frame = CGRect(x: CGFloat(passwordTextField.frame.size.width - 25), y: CGFloat(5), width: CGFloat(25), height: CGFloat(25))
         toggleButton.addTarget(self, action: #selector(self.passwordToggled(_:)), for: .touchUpInside)
@@ -142,9 +132,9 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         passwordTextField.rightView = toggleButton
         passwordTextField.rightViewMode = .always
     }
-    
-    @ objc func passwordToggled(_: UIButton) {
-        
+
+    @objc func passwordToggled(_: UIButton) {
+
         passwordTextField.isSecureTextEntry.toggle()
         if passwordTextField.isSecureTextEntry == true {
             toggleButton.setImage(hidePasswordImage, for: .normal)
@@ -152,10 +142,9 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             toggleButton.setImage(showPasswordImage, for: .normal)
         }
     }
-    
+
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         self.view.endEditing(true)
         return false
     }
 }
-
